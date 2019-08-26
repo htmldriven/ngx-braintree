2ee99d04125d6d6d59357a435ccc827f56b13350
VeselyT <tomas@vesely.in>
2019-08-26 15:41:00 +0200

    Update dropin UI; add 3ds2 support (#4)

---
+++ projects/ngx-braintree/src/lib/model/billing-address-model.ts
@@ -0,0 +1,11 @@
+export interface BillingAddress {
+  givenName?: string;
+  surname?: string;
+  phoneNumber?: string;
+  streetAddress?: string;
+  extendedAddress?: string;
+  locality?: string;
+  region?: string;
+  postalCode?: string;
+  countryCodeAlpha2?: string;
+}
