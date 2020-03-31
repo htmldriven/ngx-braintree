1864dfbfae42b072c244b4d7249535b7f7a72183
VeselyT <tomas@vesely.in>
2020-03-31 10:23:44 +0200

    Add NgxBraintreeDisableDuringProcessingDirective; Emit errors (#8)
    * Add DisableDuringProcessing directive
    
    * Emit errors
    
    * Use `emit` instead of `next`
    
    Co-authored-by: Tomas Vesely <tomas.vesely@goodvisionlive.com>

--- projects/ngx-braintree/src/lib/ngx-braintree.module.ts
+++ projects/ngx-braintree/src/lib/ngx-braintree.module.ts
@@ -6,6 +6,7 @@ import { ConfigureDropinService } from './configure-dropin.service';
 
 import { NgxBraintreeComponent } from './ngx-braintree.component';
 import { NgxBraintreeDirective } from './ngx-braintree.directive';
+import { NgxBraintreeDisableDuringProcessingDirective } from './ngx-braintree-disable-during-processing.directive';
 import { NgxBraintreePayDirective } from './ngx-braintree-pay.directive';
 
 @NgModule({
@@ -15,10 +16,12 @@ import { NgxBraintreePayDirective } from './ngx-braintree-pay.directive';
   declarations: [
     NgxBraintreeComponent,
     NgxBraintreeDirective,
+    NgxBraintreeDisableDuringProcessingDirective,
     NgxBraintreePayDirective
   ],
   exports: [
     NgxBraintreeComponent,
+    NgxBraintreeDisableDuringProcessingDirective,
     NgxBraintreePayDirective
   ],
   providers: [
